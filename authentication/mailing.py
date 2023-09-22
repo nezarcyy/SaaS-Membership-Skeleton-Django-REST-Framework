86e7ff53cb13c7bde5df8c45acc65cbce6d63ff8
nezarcyy <n.as09391259@gmail.com>
2023-09-23 00:08:53 +0100

    001

--- authentication/mailing.py
+++ authentication/mailing.py
@@ -6,3 +6,17 @@ class ActivationEmail(email.ActivationEmail):
 
 class ConfirmationEmail(email.ConfirmationEmail):
     template_name = 'authentication/ConfirmationEmail.html'
+
+class PasswordResetEmail(email.PasswordResetEmail):
+    template_name = "authentication/PasswordResetEmail.html"
+
+class PasswordChangedConfirmationEmail(email.PasswordChangedConfirmationEmail):
+    template_name = "authentication/PasswordChangedConfirmation.html"
+
+
+class UsernameChangedConfirmationEmail(email.UsernameChangedConfirmationEmail):
+    template_name = "authentication/UsernameChangedConfirmation.html"
+
+
+class UsernameResetEmail(email.UsernameResetEmail):
+    template_name = "authentication/UsernameResetEmail.html"
